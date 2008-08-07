6fa3cefa3c084177087dfcb4fc1f7165a36c9ec0
deveiant <deveiant@9e37cee1-ff4c-dd11-94a8-00304827d434>
2008-08-07 15:50:54 +0000

    Made :rdoc task use Darkfish if it exists, but not fall on its face if it doesn't.
    git-svn-id: svn://deveiate.org/rake-tasklibs/trunk@20 9e37cee1-ff4c-dd11-94a8-00304827d434

--- rdoc.rb
+++ rdoc.rb
@@ -4,27 +4,27 @@
 # 
 
 require 'rake/rdoctask'
+$have_darkfish = false
 
 begin
-	gem 'darkfish-rdoc'
-
-	Rake::RDocTask.new do |rdoc|
-		rdoc.rdoc_dir = 'docs'
-		rdoc.title    = "#{PKG_NAME} - #{PKG_SUMMARY}"
-		rdoc.options += RDOC_OPTIONS + [ '-f', 'darkfish' ]
-
-		rdoc.rdoc_files.include 'README'
-		rdoc.rdoc_files.include LIB_FILES.collect {|f| f.to_s }
-	end
-rescue LoadError, Gem::Exception => err
-	if !Object.const_defined?( :Gem )
-		require 'rubygem'
+	require 'darkfish-rdoc'
+	$have_darkfish = true
+rescue LoadError => err
+	unless Object.const_defined?( :Gem )
+		require 'rubygems'
+		gem 'darkfish-rdoc'
 		retry
 	end
 	
-	task :no_darkfish do
-		fail "Could not generate RDoc: %s" % [ err.message ]
-	end
-	task :docs => :no_darkfish
+	log "No DarkFish: %s: %s" % [ err.class.name, err.message ]
+	trace "Backtrace:\n  %s" % [ err.backtrace.join("\n  ") ]
 end
 
+Rake::RDocTask.new do |rdoc|
+	rdoc.rdoc_dir = 'docs'
+	rdoc.title    = "#{PKG_NAME} - #{PKG_SUMMARY}"
+	rdoc.options += RDOC_OPTIONS + [ '-f', 'darkfish' ] if $have_darkfish
+
+	rdoc.rdoc_files.include 'README'
+	rdoc.rdoc_files.include LIB_FILES.collect {|f| f.to_s }
+end
